d09af28cdf41eda57361696cc30b2b916a63d2c1
Milan Hintjens <milanhin@gmail.com>
2025-10-16 10:34:11 +0200

    coordinator implemented to handle data fetching and storing

---
+++ SDAC-Elia/custom_components/SDAC_Elia/coordinator.py
@@ -0,0 +1,83 @@
+import logging
+import datetime
+import requests
+import aiohttp
+
+from typing import Any
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant import config_entries
+
+_LOGGER = logging.getLogger(__name__)
+
+class SDAC_EliaCoordinator(DataUpdateCoordinator):
+    def __init__(
+            self,
+            hass: HomeAssistant,
+            config_entry: config_entries.ConfigEntry | None
+    ) -> None:
+        super().__init__(
+            hass=hass,
+            logger=_LOGGER,
+            name="SDAC_Elia-coordinator",
+            config_entry=config_entry,
+            update_interval=datetime.timedelta(minutes=1)       # Interval for which to update coordinator data
+        )
+        self.last_fetch_time: datetime.datetime | None = None   # Time of last data fetch from Elia
+        self.last_fetch_date: datetime.date | None = None       # Date of last data fetch from Elia
+        self.SDAC_data: Any = None                              # JSON object with SDAC price data from Elia
+        self.prices: list[dict] = []                            # Filtered data with time and price pairs
+        self.current_price: float | None = None                 # Current SDAC price
+    
+    async def _async_setup(self):
+        """Run setup"""
+        _LOGGER.info("SDAC_Elia coordinator was set up")
+
+    async def _async_update_data(self) -> dict[str, Any]:
+        time_now = datetime.datetime.now()
+        date_today = datetime.date.today()
+        if self.last_fetch_date != date_today:
+            try:
+                self.SDAC_data = await self._fetch_data()
+            except Exception as err:
+                _LOGGER.error("Error fetching data from Elia: %s", err)
+                return self.data
+            
+            _LOGGER.info("SDAC prices fetched from Elia")
+            self.prices = [{"time": i["dateTime"], "price": i["price"]} for i in self.SDAC_data]  # filter data to store time and price
+            self.last_fetch_time = time_now
+            self.last_fetch_date = date_today
+        
+        self.current_price = self.get_current_price()  # Always update current price
+
+        data = {
+            "prices": self.prices,
+            "current_price": self.current_price,
+            "last_fetch_time": self.last_fetch_time
+        }
+        return data
+    
+    async def _fetch_data(self) -> Any:
+        time_now = datetime.datetime.now()
+        date_today = time_now.date()
+        url = f"https://griddata.elia.be/eliabecontrols.prod/interface/Interconnections/daily/auctionresultsqh/{date_today}"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                payload = await resp.json()
+                return payload
+    
+    def get_current_price(self) -> float | None:
+        utc_time = datetime.datetime.now(datetime.timezone.utc)                                     # Get current UTC time
+        rounded_quarter = utc_time.minute // 15 * 15                                                # determine last quarter minutes
+        rounded_utc_time = utc_time.replace(microsecond=0, second=0, minute=rounded_quarter)        # change current minutes to last quarter
+        target_time_str = rounded_utc_time.strftime("%Y-%m-%dT%H:%M:%SZ")                           # Create string to match standard
+        current_price_dict = next((p for p in self.prices if p["time"] == target_time_str), None)   # Get time matching price dict
+        if current_price_dict == None:
+            _LOGGER.error("No time match found in prices from Elia")
+            return None
+        current_price = current_price_dict["price"]
+        return current_price
--- SDAC-Elia/custom_components/SDAC_Elia/sensor.py
+++ SDAC-Elia/custom_components/SDAC_Elia/sensor.py
@@ -4,7 +4,6 @@ import datetime
 import requests
 import logging
 
-#from .const import ELIA_URL
 from typing import Any
 from homeassistant.components.sensor import (
     SensorDeviceClass,
@@ -15,91 +14,45 @@ from homeassistant.const import CURRENCY_EURO
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from .coordinator import SDAC_EliaCoordinator
 
 _LOGGER = logging.getLogger(__name__)
-SCAN_INTERVAL = datetime.timedelta(minutes=1)  # Time between calling update() function
 
-def setup_platform(
+async def async_setup_platform(
     hass: HomeAssistant,
     config: ConfigType,
-    add_entities: AddEntitiesCallback,
+    async_add_entities: AddEntitiesCallback,
     discovery_info: DiscoveryInfoType | None = None
 ) -> None:
     """Set up the sensor platform."""
-    add_entities([EliaSensor()], update_before_add=True)  # True argument makes update() happen on startup
+    sdac_coordinator = SDAC_EliaCoordinator(hass=hass, config_entry=None)
+    await sdac_coordinator.async_refresh()  # Call _async_update_data() on setup
+    async_add_entities([EliaSensor(sdac_coordinator)])
     _LOGGER.info("SDAC_Elia sensor was set up")
 
 
-class EliaSensor(SensorEntity):
+class EliaSensor(CoordinatorEntity, SensorEntity): # type: ignore
     """Representation of a Sensor."""
 
     _attr_name = "Elia SDAC current price"                      # Name of sensor
     _attr_native_unit_of_measurement = f"{CURRENCY_EURO}/MWh"   # Unit of state value
     _attr_state_class = SensorStateClass.MEASUREMENT
 
-    def __init__(self):
-        super().__init__()
-        self._last_update_time: datetime.datetime | None = None  # Time of last updated state of sensor
-        self._last_fetch_time: datetime.datetime | None = None   # Time of last data fetch from Elia
-        self._last_fetch_date: datetime.date | None = None       # Date of last data fetch from Elia
-        self._SDAC_data: Any = None                              # JSON object with SDAC price data from Elia
-        self._prices: list[dict] = []                            # Filtered data with time and price pairs
-        self._current_price: float | None = None                 # Current SDAC price
+    def __init__(self, coordinator: SDAC_EliaCoordinator) -> None:
+        super().__init__(coordinator)
+        self.coordinator = coordinator
     
     @property
     def native_value(self) -> float | None: # type: ignore[override]
         """Return the current SDAC price so it gets stored in the sensor as value"""
-        return self._current_price
+        return self.coordinator.current_price
     
     @property
     def extra_state_attributes(self) -> dict[str, Any]: # type: ignore[override]
         """Store all SDAC prices of the day."""
         return {
-            "Last update:": self._last_fetch_time,
-            "prices": self._prices,
+            "Last update:": self.coordinator.last_fetch_time,
+            "prices": self.coordinator.prices,
             }
-
-    def update(self) -> None:
-        """Fetch new state data for the sensor.
-
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        
-        time_now = datetime.datetime.now()
-        date_today = datetime.date.today()
-        if self._last_fetch_date != date_today:
-            try:
-                self._SDAC_data = self._fetch_data()
-            except Exception as err:
-                _LOGGER.error("Error fetching data from Elia: %s", err)
-                return
-            
-            _LOGGER.info("SDAC prices fetched from Elia")
-            self._prices = [{"time": i["dateTime"], "price": i["price"]} for i in self._SDAC_data]  # filter data to store time and price
-            self._last_fetch_time = time_now
-            self._last_fetch_date = date_today
-
-        self._current_price = self.get_current_price()
-        _LOGGER.info("SDAC_Elia sensor value updated")
-
-    def get_current_price(self) -> float | None:
-        utc_time = datetime.datetime.now(datetime.timezone.utc)                                     # Get current UTC time
-        rounded_quarter = utc_time.minute // 15 * 15                                                # determine last quarter minutes
-        rounded_utc_time = utc_time.replace(microsecond=0, second=0, minute=rounded_quarter)        # change current minutes to last quarter
-        target_time_str = rounded_utc_time.strftime("%Y-%m-%dT%H:%M:%SZ")                           # Create string to match standard
-        current_price_dict = next((p for p in self._prices if p["time"] == target_time_str), None)  # Get time matching price dict
-        if current_price_dict == None:
-            _LOGGER.error("No time match found in prices from Elia")
-            return None
-        current_price = current_price_dict["price"]
-        return current_price
-
-    def _fetch_data(self) -> Any:
-        time_now = datetime.datetime.now()
-        date_today = time_now.date()
-        url = f"https://griddata.elia.be/eliabecontrols.prod/interface/Interconnections/daily/auctionresultsqh/{date_today}"
-        response = requests.get(url, timeout=5)  # Get payload from Elia database
-        response.raise_for_status()
-        _LOGGER.info("Elia SDAC prices fetched")
-        data = response.json()
-        return data
